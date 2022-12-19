7154fb43f3ad8ef979daae8b773c99e9b629d449
alex Mendoza <tiburon9386@gmail.com>
2022-12-19 16:06:06 -0600

    Change api key

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const KEY = "AIzaSyD7WKuDF-sH-Oj6uVmeYUf0vRiUo-QgAGM";
+const KEY = "INSERT-YOUTUBE-API-KEY";
 
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3",
