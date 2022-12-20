58e267c3ea39618f9142471292d3ad771f719092
alex Mendoza <tiburon9386@gmail.com>
2022-12-20 11:51:12 -0600

    colums

--- src/components/App.js
+++ src/components/App.js
@@ -4,47 +4,51 @@ import youtube from "../apis/youtube";
 import VideoList from "./VideoList";
 import VideoDetail from "./VideoDetail";
 
-class App extends React.Component{
-    state = {videos: [], selectedVideo: null};
+class App extends React.Component {
+  state = { videos: [], selectedVideo: null };
 
-    componentDidMount(){
-        this.onTermSubmit('base jumping')
-    }
+  componentDidMount() {
+    this.onTermSubmit("base jumping");
+  }
 
-    onTermSubmit = async term => {
-        const response = await youtube.get('/search',{
-            params:{
-                q: term
-            }
-        });
-        console.log(response);
-        this.setState({ 
-            videos: response.data.items,
-            selectedVideo: response.data.items[0]
-         });
+  onTermSubmit = async (term) => {
+    const response = await youtube.get("/search", {
+      params: {
+        q: term,
+      },
+    });
+    console.log(response);
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0],
+    });
+  };
 
-    };
+  onVideoSelect = (video) => {
+    this.setState({ selectedVideo: video });
+  };
 
-    onVideoSelect = (video) => {
-        this.setState({selectedVideo: video});
-    }
-
-    render(){
-        return (
-        <div className="ui container">
-            <SearchBar onFormSubmit={this.onTermSubmit}/>
-            se encontraron {this.state.videos.length} videos
-            <div className="ui row">
-                <div className="seven wide column">
-                <VideoDetail video={this.state.selectedVideo}/>
-                </div>
-                <div className="five wide column">
-                <VideoList videos={this.state.videos} onVideoSelect={this.onVideoSelect}/>
-                </div>
-            </div>           
+  render() {
+    return (
+      <div className="ui container">
+        <SearchBar onFormSubmit={this.onTermSubmit} />
+        se encontraron {this.state.videos.length} videos
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className="five wide column">
+              <VideoList
+                videos={this.state.videos}
+                onVideoSelect={this.onVideoSelect}
+              />
+            </div>
+          </div>
         </div>
-        )
-    }
+      </div>
+    );
+  }
 }
 
 export default App;
